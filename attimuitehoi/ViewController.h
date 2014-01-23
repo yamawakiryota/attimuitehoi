755d20bd72c8a87d21b01dae78f3abb0430f7a72
yamawaki ryota <s1531070@eng.kitakyu-u.ac.jp>
2014-01-23 15:51:23 +0900

    途中！

--- attimuitehoi/ViewController.h
+++ attimuitehoi/ViewController.h
@@ -9,5 +9,29 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *labelOne;
+
+- (IBAction)resetbotton:(id)sender;
+- (IBAction)ue:(id)sender;
+- (IBAction)migi:(id)sender;
+- (IBAction)hidari:(id)sender;
+- (IBAction)sita:(id)sender;
+- (IBAction)gu:(id)sender;
+- (IBAction)tyoki:(id)sender;
+- (IBAction)pa:(id)sender;
+@property (weak, nonatomic) IBOutlet UIButton *gubotton;
+@property (weak, nonatomic) IBOutlet UIButton *chokibotton;
+@property (weak, nonatomic) IBOutlet UIButton *pabotton;
+@property (weak, nonatomic) IBOutlet UIButton *uebotton;
+@property (weak, nonatomic) IBOutlet UIButton *migibotton;
+@property (weak, nonatomic) IBOutlet UIButton *hidaribotton;
+@property (weak, nonatomic) IBOutlet UIButton *sitabotton;
+
+- (void)enemy;
+- (void)janken;
+- (void)win;
+- (void)lose;
+- (void)draw;
+- (void)attimuite;
 
 @end
--- attimuitehoi/ViewController.m
+++ attimuitehoi/ViewController.m
@@ -7,8 +7,16 @@
 //
 
 #import "ViewController.h"
+#define GU 0
+#define CHOKI 1
+#define PA 2
 
-@interface ViewController ()
+
+@interface ViewController (){
+    int x;
+    int e;
+    
+}
 
 @end
 
@@ -17,7 +25,12 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	
+    self.labelOne.text = @"じゃんけん・・・";
+    self.uebotton.hidden = YES;
+    self.migibotton.hidden = YES;
+    self.sitabotton.hidden = YES;
+    self.hidaribotton.hidden = YES;
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +39,114 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)resetbotton:(id)sender {
+    self.labelOne.text = @"じゃんけん・・・";
+    self.uebotton.hidden = YES;
+    self.migibotton.hidden = YES;
+    self.sitabotton.hidden = YES;
+    self.hidaribotton.hidden = YES;
+    self.gubotton.hidden = NO;
+    self.chokibotton.hidden = NO;
+    self.pabotton.hidden = NO;
+    x = 0,e = 0;
+}
+
+
+
+- (IBAction)ue:(id)sender {
+}
+
+- (IBAction)migi:(id)sender {
+}
+
+- (IBAction)hidari:(id)sender {
+}
+
+- (IBAction)sita:(id)sender {
+}
+
+- (IBAction)gu:(id)sender {
+    x = GU;
+    
+    [self janken];
+}
+
+- (IBAction)tyoki:(id)sender {
+    x = CHOKI;
+    [self janken];
+}
+
+- (IBAction)pa:(id)sender {
+    x = PA;
+    [self janken];
+}
+
+- (void)enemy{
+    e = arc4random() % 3;
+    
+}
+
+-(void)janken{
+    [self enemy];
+    
+    if(x == e){
+        [self draw];
+}
+    if(x==GU && e==CHOKI){
+    [self win];
+}
+    if(x==CHOKI && e==PA){
+    [self win];
+}
+    if(x==PA && e==GU){
+        [self win];
+    }
+    
+    if(x==GU && e==PA){
+    [self lose];
+}
+    if(x==CHOKI && e==GU){
+    [self lose];
+}
+    if(x==PA && e==CHOKI){
+        [self lose];
+}
+}
+
+- (void)win{
+    self.uebotton.hidden = NO;
+    self.migibotton.hidden = NO;
+    self.sitabotton.hidden = NO;
+    self.hidaribotton.hidden = NO;
+    self.gubotton.hidden = YES;
+    self.chokibotton.hidden = YES;
+    self.pabotton.hidden = YES;
+    self.labelOne.text = @"あっち向いて・・・";}
+
+- (void)lose{
+    self.labelOne.text = @"あっち向いて・・・";
+    self.uebotton.hidden = NO;
+    self.migibotton.hidden = NO;
+    self.sitabotton.hidden = NO;
+    self.hidaribotton.hidden = NO;
+    self.gubotton.hidden = YES;
+    self.chokibotton.hidden = YES;
+    self.pabotton.hidden = YES;
+}
+
+- (void)draw{
+    self.labelOne.text = @"じゃんけん・・・";
+    self.uebotton.hidden = YES;
+    self.migibotton.hidden = YES;
+    self.sitabotton.hidden = YES;
+    self.hidaribotton.hidden = YES;
+    self.gubotton.hidden = NO;
+    self.chokibotton.hidden = NO;
+    self.pabotton.hidden = NO;
+    x = 0,e = 0;
+}
+
+- (void)attimuite{
+    
+}
 @end
